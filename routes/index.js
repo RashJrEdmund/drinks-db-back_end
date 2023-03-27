9019290082d3af053ed6f78c106046efde60d2f9
rash <orashusedmund@gmail.com>
2023-03-27 10:10:54 +0100

    modified utils and index.js

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,16 @@ router.post('/login', async (req, res) => {
 
   console.log('\n next part entered \n', result, email, password);
 
-  if(result.status === 401) res.sendStatus(401); // since loginWith... returns either a { status: 401 } || the user and the created user and his token
+  if(result.status === 401) return res.status(401).send(result.message); // since loginWith... returns either a { status: 401 } || the user and the created user and his token
 
-  res.send(result.token);
+  const { createdAt } = result.user;
+  const day = createdAt.split('-').pop().split('T').shift();
+  const month = createdAt.split('-')[1];
+  const year = createdAt.split('-')[0]
+
+  const joinedSince = 
+
+  res.send(result);
 });
 
 module.exports = router;
--- services/utils.js
+++ services/utils.js
@@ -11,13 +11,13 @@ const loginWithEmailPassword = async (email, password) => {
     }
   });
 
-  if(!user) return { status: 401, message: 'user not found' };
+  if(!user) return { status: 401, message: 'incorrect logins' };
 
   user = user.dataValues || user; // since users gives a complex object(proxy) with unneccesary key value pairs
 
   const match = await bcrypt.compare(password, user.password);
 
-  if(!match) return { status: 401 };
+  if(!match) return { status: 401, message: 'incorrect logins' };
 
   const token = jwt.sign(
     { barer_id: user.id, barer_email: user.email }, // giving the token barer his normal id, and email as barer_id / barer_email so i'll use it as reference when i want to log in with token
